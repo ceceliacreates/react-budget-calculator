c917728f8fac426b43424e6187c8a5bd55843a1e
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-05-26 19:14:02 -0400

    Updates

---
+++ precache-manifest.134472f6a4cd7cf28dbc2f559ce7dfab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a927580bc8b29a58e076bf978a1c70",
+    "url": "/react-budget-calculator/index.html"
+  },
+  {
+    "revision": "65521c98693dedf0fa66",
+    "url": "/react-budget-calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-budget-calculator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "65521c98693dedf0fa66",
+    "url": "/react-budget-calculator/static/js/main.0b6c5411.chunk.js"
+  },
+  {
+    "revision": "c06d89111354a56877f2",
+    "url": "/react-budget-calculator/static/js/runtime~main.01ca1d82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-budget-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
