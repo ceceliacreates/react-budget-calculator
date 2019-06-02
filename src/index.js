bb6e8522a53150b22e660e95567e6b7e84398766
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-06-02 17:13:32 -0400

    started styling form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import "./index.css";
+import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 class Step1 extends React.Component {
