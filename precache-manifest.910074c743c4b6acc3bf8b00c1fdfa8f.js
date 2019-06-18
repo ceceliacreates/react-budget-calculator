9d347aa97eeadd9bb0a0bf90f4b1662d9182196f
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-06-18 11:31:41 -0400

    Updates

--- precache-manifest.910074c743c4b6acc3bf8b00c1fdfa8f.js
+++ precache-manifest.910074c743c4b6acc3bf8b00c1fdfa8f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "337ca9bfb73dd99a7659a437b193fe5f",
+    "revision": "81caa5d6d49469da83d8e3be15d9cb4a",
     "url": "/react-budget-calculator/index.html"
   },
   {
-    "revision": "ea461e8756df61bad6f1",
-    "url": "/react-budget-calculator/static/css/main.718dd92c.chunk.css"
+    "revision": "ce8ddceb815f79d85b99",
+    "url": "/react-budget-calculator/static/css/main.61f0986d.chunk.css"
   },
   {
     "revision": "f0b521c66bc702a37631",
     "url": "/react-budget-calculator/static/js/2.9f4f2337.chunk.js"
   },
   {
-    "revision": "ea461e8756df61bad6f1",
-    "url": "/react-budget-calculator/static/js/main.a10f4d3c.chunk.js"
+    "revision": "ce8ddceb815f79d85b99",
+    "url": "/react-budget-calculator/static/js/main.5ca24f74.chunk.js"
   },
   {
     "revision": "c06d89111354a56877f2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-budget-calculator/precache-manifest.d3012af23f154ecb7201565805a9c16f.js"
+  "/react-budget-calculator/precache-manifest.910074c743c4b6acc3bf8b00c1fdfa8f.js"
 );
 
 self.addEventListener('message', (event) => {
